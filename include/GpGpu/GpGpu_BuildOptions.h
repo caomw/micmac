a584e6987de49be0ef2abc2253777f704444beb7
Helio Chissini de Castro <helio@kde.org>
2015-02-12 18:03:48 -0200

    - Initial fork import from 20140212 codebase on culture3d mercurial

---
+++ include/GpGpu/GpGpu_BuildOptions.h
@@ -0,0 +1,7 @@
+/* #undef CUDA_ENABLED */
+/* #undef OPM_ENABLED */
+#define OPENCL_ENABLED  1
+/* #undef CPP11THREAD_NOBOOSTTHREAD */
+//#ifndef __CUDA_API_VERSION
+/* #undef __CUDA_API_VERSION */
+//#endif
---
+++ include/general/CMake_defines.h
@@ -0,0 +1,8 @@
+#ifndef ELISE_QT_VERSION
+#define ELISE_QT_VERSION 4
+#endif
+
+#define ELISE_DEPLOY 0
+#define PROJECT_SOURCE_DIR "/code/culture3d"
+
+#define ELISE_PTR_SIZE 8
